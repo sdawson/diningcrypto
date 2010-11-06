4c94dcef7878cb0e908cb63e00a54ab7373b69fa
Sophie Dawson <sophiecdawson@gmail.com>
2010-11-06 18:02:13 +1100

    Setting up modular arithmetic for manip of chars.
    Need to be able to send any alphabetic character,
    as well as a null (used @ in this version, due to its
    position in ascii, but can be hidden behind a simpler
    abstraction).  Need to be able to handle multiple
    keys/keyops for a single encoding, as the number of
    keys shared between clients is variable.

--- Dining Crypto/src/communication/Message.java
+++ Dining Crypto/src/communication/Message.java
@@ -5,12 +5,42 @@ import java.io.Serializable;
 public class Message implements Serializable {
 	private static final long serialVersionUID = -4317983621836587347L;
 	private String message;
+	// Individual message chars are just stored as offsets
+	private char indMessage;
+	private static final int base = (int) '@'; // @ represents no msg
+	private static final int alphaSize = 27;
 	
 	public Message(String message) {
 		this.message = message;
 	}
 	
+	public Message(char indMessage) {
+		this.indMessage = (char) (indMessage - base);
+	}
+	
 	public String getMessage() {
 		return this.message;
+	}
+	
+	public char getIndMessage() {
+		return (char) (this.indMessage + base);
+	}
+	
+	public void encode(char key, Keyop op) {
+		int keyOffset = key - base;
+		if (op == Keyop.ADD) {
+			System.out.println("key offset " + keyOffset);
+			System.out.println("added key offset " + (this.indMessage + keyOffset));
+			this.indMessage = (char) ((this.indMessage + keyOffset) % alphaSize);
+			System.out.println("final message (with unneccessary +base) " + (int) this.indMessage);
+		} else if (op == Keyop.SUBTRACT) {
+			System.out.println("subtraction with mod " + ((this.indMessage - keyOffset) % alphaSize));
+			System.out.println("subtraction, mod, +base " + (((this.indMessage - keyOffset) + alphaSize) % alphaSize));
+			System.out.println("subtraction, mod, +base " + (((7 - 5) + alphaSize) % alphaSize));
+			this.indMessage = (char) (((this.indMessage - keyOffset) + alphaSize) % alphaSize);
+		} else {
+			System.err.println("Error: Invalide encoding operation requested.");
+			System.exit(1);
+		}
 	}
 }
