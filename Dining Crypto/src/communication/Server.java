01d93aa240a031a0cca13324f923787262e44ff0
Sophie Dawson <sophiecdawson@gmail.com>
2010-11-07 22:11:09 +1100

    Adjustment to store all client connection info.

--- Dining Crypto/src/communication/Server.java
+++ Dining Crypto/src/communication/Server.java
@@ -6,21 +6,17 @@ import java.util.ArrayList;
 
 public class Server {
 	private static final int PORT = 9876;
-	private ArrayList<Socket> clients = new ArrayList<Socket>();
+	private static ArrayList<ClientSocketInfo> clients = new ArrayList<ClientSocketInfo>();
 	
 	public static void main(String[] args) throws IOException {
 		ServerConnection connection = new ServerConnection(PORT);
 		
-		ClientSocketInfo first = connection.acceptConnection();
-		
-		/*Message stuff = (Message) connection.getInputStream().readObject();
-		System.out.println("received " + stuff.getMessage());
-		Message finalMessage = new Message("fldkfjs");
-		connection.getOutputStream().writeObject(finalMessage);*/
-		Message stuff = connection.receive(first);
+		clients.add(connection.acceptConnection());
+
+		Message stuff = connection.receive(clients.get(0));
 		System.out.println("received " + stuff.getMessage());
 		Message finalMessage = new Message("working recv/send server connection.");
-		connection.send(finalMessage, first);
+		connection.send(finalMessage, clients.get(0));
 
 	}
 }
