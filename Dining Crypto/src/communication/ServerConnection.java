6e627b7ea6389aaa3a27b5609a0e4656028f4996
Sophie Dawson <sophiecdawson@gmail.com>
2010-11-07 22:11:32 +1100

    Code to allow sending keys to clients, which requires ack.

--- Dining Crypto/src/communication/ServerConnection.java
+++ Dining Crypto/src/communication/ServerConnection.java
@@ -5,6 +5,9 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Map;
 
 public class ServerConnection {
 	private ServerSocket serverSocket = null;
@@ -44,6 +47,25 @@ public class ServerConnection {
 	
 	public void send(Message message, ClientSocketInfo csi) throws IOException {
 		csi.getOutputStream().writeObject(message);
+	}
+
+	public void sendKeysWithAck(Map<ClientSocketInfo, KeySet> keyMap) throws IOException {
+		Iterator<Map.Entry<ClientSocketInfo, KeySet>> iterate = keyMap.entrySet().iterator();
+		while (iterate.hasNext()) {
+			Map.Entry<ClientSocketInfo, KeySet> entry =
+				(Map.Entry<ClientSocketInfo, KeySet>) iterate.next();
+			entry.getKey().getOutputStream().writeObject(keyMap);
+			// Wait for ack before sending the next object
+			Message reply = receive(entry.getKey());
+			if (reply.getMessage().equals(new String("OK"))) {
+				// successful ack
+				continue;
+			} else {
+				// wait??  Or try to resend.....
+				System.err.println("Error: Sending key to client failed");
+				System.exit(1);
+			}
+		}
 	}
 	
 	public void disconnect() throws IOException {
