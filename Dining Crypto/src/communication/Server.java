ce043efaa06434f73ffc3419616a91118f9c0964
Sophie Dawson <sophiecdawson@gmail.com>
2010-11-12 23:01:34 +1100

    Setting the server to take a limited number of clients.
    This is needed to ensure that the process of distributing
    keys can actually occur.  A more useful way of doing
    this would be if the client program/protocol had a
    message to indicate that the last client is connecting.
    The server could just wait for this message, then
    start doing the key generation, assignment, 
    rounds/message passing etc, since it knows that no
    more clients are going to enter the picture.

--- Dining Crypto/src/communication/Server.java
+++ Dining Crypto/src/communication/Server.java
@@ -4,16 +4,18 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 public class Server {
-	public static final int PORT = 9876;
+	private static final int PORT = 9876;
+	private static final int MAXCLIENTS = 3;
 	
 	public static void main(String[] args) throws IOException {
 		ServerConnection connection = new ServerConnection(PORT);
 		ArrayList<ClientSocketInfo> clients = new ArrayList<ClientSocketInfo>();
 		int count = 0;
 		
-		while (true) {
+		/* Only accept three clients */
+		for (int i=0; i<MAXCLIENTS; i++) {
 			clients.add(connection.acceptConnection());
-			new ServerThread(clients, count).start();
+			new ServerThread(clients, i, MAXCLIENTS).start();
 			count++;
 		}
 	}
--- Dining Crypto/src/communication/ServerThread.java
+++ Dining Crypto/src/communication/ServerThread.java
@@ -7,10 +7,13 @@ import java.util.ArrayList;
 public class ServerThread extends Thread {
 	private ArrayList<ClientSocketInfo> clients = null;
 	private int clientID;
+	private int maxClients;
 	
-	public ServerThread(ArrayList<ClientSocketInfo> clients, int clientID) {
+	public ServerThread(ArrayList<ClientSocketInfo> clients, int clientID,
+			int maxClients) {
 		this.clients = clients;
 		this.clientID = clientID;
+		this.maxClients = maxClients;
 	}
 	
 	public void run() {
@@ -18,6 +21,12 @@ public class ServerThread extends Thread {
 		Message ack = new Message("OK");
 		System.err.println("In a server thread (right at the start)");
 		ClientSocketInfo clientConnection = clients.get(clientID);
+		System.out.println("perceived size of client socket array: " + clients.size());
+		/* Want to start doing message passing rounds once a decent number (3 for a start)
+		 * of clients have actually connected.  This limit can be changed, but need
+		 * to stop letting them in at some stage so that the server can calculate
+		 * key-pairs etc.
+		 */
 		try {
 			// Acknowledge first connection, so that the client can keep sending
 			// more messages
@@ -27,6 +36,26 @@ public class ServerThread extends Thread {
 				stuff = clientConnection.receive();
 				if (stuff.getMessage().equals("KILL"))
 					break;
+
+				if (clients.size() == maxClients) {
+					// send the message around the chain
+					// to the client above your own number
+					// (so that the sockets don't duplicate the sending
+					System.out.println("received " + stuff.getMessage());
+					if ((clientID + 1) == clients.size()) {
+						// Need to send back to the first client (making array circular)
+						clients.get(0).send(stuff);
+						System.out.println("Thread " + clientID + " sending to client 0");
+					} else {
+						clients.get(clientID+1).send(stuff);
+						System.out.println("Thread " + clientID + " sending to client " + clientID+1);
+					}
+				} else {
+					// Keep waiting until all the clients have connected
+					continue;
+				}
+				/*if (stuff.getMessage().equals("KILL"))
+					break;
 				System.out.println("received " + stuff.getMessage());
 				ArrayList<ClientSocketInfo> otherClients =
 					new ArrayList<ClientSocketInfo>(clients);
@@ -35,10 +64,11 @@ public class ServerThread extends Thread {
 				for (ClientSocketInfo c : otherClients) {
 					// Broadcast the message just received
 					c.send(stuff);
-				}
+				}*/
 			}
 			System.out.println("Outside while loop");
 			Message finalMessage = new Message("END");
+			System.out.println("perceived size of client socket array: " + clients.size());
 			for (ClientSocketInfo c : clients) {
 				System.err.println("sending a shutdown message");
 				c.send(finalMessage);
