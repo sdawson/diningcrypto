29dec5642e69b54335e92c1de64703e90a268f9f
Joshua Torrance <joshuatorrance@gmail.com>
2010-11-16 13:05:13 +1100

    Cleaned up the imports that were giving warnings.

--- Dining Crypto/src/communication/Message.java
+++ Dining Crypto/src/communication/Message.java
@@ -1,7 +1,6 @@
 package communication;
 
 import java.io.Serializable;
-import java.util.ArrayList;
 import java.util.List;
 
 public class Message implements Serializable {
--- Dining Crypto/src/server/ServerConnection.java
+++ Dining Crypto/src/server/ServerConnection.java
@@ -1,10 +1,7 @@
 package server;
 
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
-import java.net.Socket;
 
 
 public class ServerConnection {
