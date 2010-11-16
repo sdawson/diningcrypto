60cdb659e64d0ca030e012c4a9e37ada42ccb8ee
Joshua Torrance <joshuatorrance@gmail.com>
2010-11-16 13:42:35 +1100

    Added random key generator to Key. Changed key data to be int rather than char.

--- Dining Crypto/src/communication/Key.java
+++ Dining Crypto/src/communication/Key.java
@@ -1,19 +1,38 @@
 package communication;
 
+import java.security.SecureRandom;
+
 public class Key {
-	private final char key;
+	private final int key;
 	private final Keyop op;
 	
-	public Key(char key, Keyop op) {
+	public Key(int key, Keyop op) {
 		this.key = key;
 		this.op = op;
 	}
 	
-	public char getKey() {
+	public int getKey() {
 		return this.key;
 	}
 	
 	public Keyop getKeyop() {
 		return this.op;
+	}
+	
+	/**
+	 * Generates a random key with a key operation of null.
+	 */
+	public static Key generateRandomKey() {
+		SecureRandom rand = new SecureRandom();
+		
+		byte[] b = new byte[4];
+		rand.nextBytes(b);
+		
+		int i = (new Byte(b[0])).intValue() +
+				(new Byte(b[1])).intValue()*Byte.MAX_VALUE +
+				(new Byte(b[1])).intValue()*Byte.MAX_VALUE*Byte.MAX_VALUE +
+				(new Byte(b[1])).intValue()*Byte.MAX_VALUE*Byte.MAX_VALUE*Byte.MAX_VALUE;
+		
+		return new Key(i, null);
 	}
 }
