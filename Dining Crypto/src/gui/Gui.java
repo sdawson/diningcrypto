f57c31b5eff00ff47b8e9a4303a0576934d6ec40
Joshua Torrance <joshuatorrance@gmail.com>
2010-11-16 11:14:51 +1100

    Added stuff so that the client can close tidyly.

--- Dining Crypto/src/gui/Gui.java
+++ Dining Crypto/src/gui/Gui.java
@@ -3,6 +3,8 @@ package gui;
 import interfaces.Output;
 
 import java.awt.BorderLayout;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -10,7 +12,7 @@ import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
-public class Gui extends JFrame implements Output {
+public class Gui extends JFrame implements Output, WindowListener {
 	private static final long serialVersionUID = -7886147914342084854L;
 
 	private JTextField textSubmit;
@@ -23,6 +25,7 @@ public class Gui extends JFrame implements Output {
 		super(title);
 		
 		this.submitAct = submitAct;
+		addWindowListener(this);
 		
 		setLayout(new BorderLayout());
 	
@@ -70,5 +73,48 @@ public class Gui extends JFrame implements Output {
 	@Override
 	public void outputString(String str) {
 		displayText(str);
+	}
+
+	@Override
+	public void windowActivated(WindowEvent arg0) {
+		// Do nothing special
+		
+	}
+
+	@Override
+	public void windowClosed(WindowEvent arg0) {
+		// Do nothing special
+		
+	}
+
+	@Override
+	public void windowClosing(WindowEvent arg0) {
+		// Do nothing special
+		// TODO: actually communicate this to something
+		System.out.println("Closing!!!1");
+	}
+
+	@Override
+	public void windowDeactivated(WindowEvent arg0) {
+		// Do nothing special
+		
+	}
+
+	@Override
+	public void windowDeiconified(WindowEvent arg0) {
+		// Do nothing special
+		
+	}
+
+	@Override
+	public void windowIconified(WindowEvent arg0) {
+		// Do nothing special
+		
+	}
+
+	@Override
+	public void windowOpened(WindowEvent arg0) {
+		// Do nothing special
+		
 	}
 }
